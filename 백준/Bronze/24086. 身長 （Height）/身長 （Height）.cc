c5875f293c09d2a055d5c9046c20167571f1d757
ikk5515 <ikk5515@naver.com>
2023-02-19 18:48:45 +0900

    [Bronze V] Title: 身長 (Height), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24086. 身長 （Height）/身長 （Height）.cc
@@ -0,0 +1,11 @@
+// BOJ_24086_身長 (Height)
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+    int a, b; cin >> a >> b;
+    cout << b-a;
+    return 0;
+}
