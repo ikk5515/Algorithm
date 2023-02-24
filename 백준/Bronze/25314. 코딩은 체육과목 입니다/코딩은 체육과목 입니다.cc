af907d7dc0e9429a683a12c23257565732f378d0
ikk5515 <ikk5515@naver.com>
2023-02-24 23:09:14 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.cc
@@ -0,0 +1,12 @@
+//BOJ_25314_코딩은 체육과목 입니다
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n; cin >> n;
+    for (int i = 0; i < n/4; i++) {
+        cout << "long ";
+    }
+    cout << "int";
+    return 0;
+}
