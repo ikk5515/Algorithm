b66d06d53295fd7052239522b38cd8833469b496
ikk5515 <ikk5515@naver.com>
2023-02-26 21:44:00 +0900

    [Bronze V] Title: 아스키 코드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cc
@@ -0,0 +1,11 @@
+// BOJ_11654_아스키 코드
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+    char ch; cin >> ch;
+    cout << (int)ch;
+    return 0;
+}
