4f408007dd64a3f4f7e789a823c4c4a4ada69855
ikk5515 <ikk5515@naver.com>
2023-03-04 16:04:30 +0900

    [Silver III] Title: 피보나치 수의 확장, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/1788. 피보나치 수의 확장/피보나치 수의 확장.cc
+++ 백준/Silver/1788. 피보나치 수의 확장/피보나치 수의 확장.cc
@@ -11,35 +11,20 @@ int main() {
     int a = abs(n);
     dp[0] = 0;
     dp[1] = 1;
-
     for (int i = 2; i <= a; i++) {
         dp[i] = (dp[i - 1] + dp[i - 2])%1000000000;
     }
-    if (n < 0) {
+    if (n > 0) {
+        cout << 1 << "\n";
+    } else if (n < 0) {
         if (n%2 == 0) {
             cout << -1 << "\n";
         } else {
             cout << 1 << "\n";
         }
-    } else if (n > 0) {
-        cout << 1 << "\n";
     } else {
         cout << 0 << "\n";
     }
     cout << dp[a];
     return 0;
 }
-/*
-n이 음수일 때 짝수인 경우 음수가 됨
-f(-5) = 5
-f(-4) = -3
-f(-3) = 2
-f(-2) = -1
-f(-1) = 1
-f(0) = 0
-f(1) = 1
-f(2) = 1
-f(3) = 2
-f(4) = 3
-f(5) = 5
-*/
