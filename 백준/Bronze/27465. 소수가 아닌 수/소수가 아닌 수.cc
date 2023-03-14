86141692f36be9f91a23cc49007e0ba992b155a0
ikk5515 <ikk5515@naver.com>
2023-03-14 22:51:11 +0900

    [Bronze III] Title: 소수가 아닌 수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27465. 소수가 아닌 수/소수가 아닌 수.cc
@@ -0,0 +1,12 @@
+// BOJ_27465_소수가 아닌 수
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+    long long n; cin>>n;
+
+    cout << 1000000000;
+    return 0;
+}
