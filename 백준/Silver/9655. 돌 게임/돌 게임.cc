6325fcb69059db1c8683f6df75b33ae32344ae39
ikk5515 <ikk5515@naver.com>
2023-03-15 23:33:33 +0900

    [Silver V] Title: 돌 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.cc
@@ -0,0 +1,16 @@
+// BOJ_9655_돌 게임
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+    int n; cin>>n;
+
+    if (n % 2 == 1) {
+        cout << "SK";
+    } else {
+        cout << "CY";
+    }
+    return 0;
+}
