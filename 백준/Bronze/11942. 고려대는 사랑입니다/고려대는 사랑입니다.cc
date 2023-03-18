d0c978a406039ed207fb8b6f16617ed96a6f9013
ikk5515 <ikk5515@naver.com>
2023-03-18 23:22:23 +0900

    [Bronze V] Title: 고려대는 사랑입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11942. 고려대는 사랑입니다/고려대는 사랑입니다.cc
@@ -0,0 +1,10 @@
+// BOJ_2738_행렬 덧셈
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+    cout << "고려대학교";
+    return 0;
+}
