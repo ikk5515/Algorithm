827cf1b96d6eaf24f065c89d7f4585e8305a438b
ikk5515 <ikk5515@naver.com>
2023-03-19 22:08:07 +0900

    [Bronze V] Title: 엔드게임 스포일러, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/17295. 엔드게임 스포일러/엔드게임 스포일러.cc
@@ -0,0 +1,10 @@
+// BOJ_17295_엔드게임 스포일러
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+    cout << "Avengers: Endgame";
+    return 0;
+}
