5ee226e05ea3e13ffe6efe89430e949452d64aba
ikk5515 <ikk5515@naver.com>
2023-04-04 23:34:02 +0900

    [Bronze V] Title: SciComLove, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/21598. SciComLove/SciComLove.cc
@@ -0,0 +1,15 @@
+// BOJ_21598_SciComLove
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+    int n; cin >> n;
+    if (n == 1) {
+        cout << "SciComLove";
+    } else {
+        cout << "SciComLove\nSciComLove";
+    }
+    return 0;
+}
