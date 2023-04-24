bffd5ab5156872366d2d4ab2daa3594be7c2778c
ikk5515 <ikk5515@naver.com>
2023-04-24 23:03:27 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.cc
@@ -0,0 +1,15 @@
+// BOJ_15680_연세대학교
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+    int n; cin >> n;
+    if (n == 0) {
+        cout << "YONSEI";
+    } else {
+        cout << "Leading the Way to the Future";
+    }
+    return 0;
+}
