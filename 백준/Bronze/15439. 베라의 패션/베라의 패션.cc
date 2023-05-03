cfb08e9b9ed8a26898d1ed80f870624104520f2b
ikk5515 <ikk5515@naver.com>
2023-05-03 23:32:59 +0900

    [Bronze IV] Title: 베라의 패션, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.cc
@@ -0,0 +1,11 @@
+// BOJ_15439_베라의 패션
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+    int n; cin >> n;
+    cout << n * (n-1);
+    return 0;
+}
