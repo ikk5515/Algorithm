cab66e27305740dfef337d747d940e0e36ef343a
ikk5515 <ikk5515@naver.com>
2023-05-06 23:49:16 +0900

    [Bronze V] Title: Copier, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26574. Copier/Copier.cc
@@ -0,0 +1,14 @@
+// BOJ_26574_Copier
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+    int n; cin >> n;
+    for (int i = 0; i < n; i++) {
+        int a; cin >> a;
+        cout << a << " " << a << "\n";
+    }
+    return 0;
+}
