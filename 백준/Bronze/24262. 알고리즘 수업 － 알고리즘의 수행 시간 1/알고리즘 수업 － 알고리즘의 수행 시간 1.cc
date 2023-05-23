5a98fc8abdda2597169966d774e25e5ccae6e087
ikk5515 <ikk5515@naver.com>
2023-05-23 23:37:18 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.cc
@@ -0,0 +1,9 @@
+// BOJ_24262_알고리즘 수업 - 알고리즘의 수행 시간 1
+#include <iostream>
+using namespace std;
+int main() {
+    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+    int n; cin >> n;
+    cout << 1 << '\n' << 0;
+    return 0;
+}
