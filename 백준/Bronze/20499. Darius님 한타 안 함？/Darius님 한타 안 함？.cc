4c4b4ea46313c823138b7ae38ae9afef4e28fe99
ikk5515 <ikk5515@naver.com>
2023-05-27 23:22:56 +0900

    [Bronze IV] Title: Darius님 한타 안 함?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/20499. Darius님 한타 안 함？/Darius님 한타 안 함？.cc
+++ 백준/Bronze/20499. Darius님 한타 안 함？/Darius님 한타 안 함？.cc
@@ -3,10 +3,11 @@
 using namespace std;
 
 int main() {
-    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
 
-    char ch;
-    int k, d, a; cin >> k >> ch >> d >> ch >> a;
+    int k,d,a;
+    scanf("%d/%d/%d", &k, &d, &a);
+
     if (k + a < d || d == 0) {
         cout << "hasu";
     } else {
