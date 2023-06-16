cc550e4ce1daa5b9c9e29436c02f240698286cce
ikk5515 <ikk5515@naver.com>
2023-06-16 22:17:31 +0900

    [Bronze V] Title: 2 桁の整数 (Two-digit Integer), Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27331. 2 桁の整数 （Two－digit Integer）/2 桁の整数 （Two－digit Integer）.cc
@@ -0,0 +1,13 @@
+// BOJ_27331_2 桁の整数 (Two-digit Integer)
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+    string s1,s2; cin >> s1 >> s2;
+
+    cout << s1 << s2;
+    return 0;
+}
