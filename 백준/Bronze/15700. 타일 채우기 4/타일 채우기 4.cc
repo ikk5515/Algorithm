4daac12f6387d2010f2d817e1b2b4a5f20482fa5
ikk5515 <ikk5515@naver.com>
2023-07-30 23:10:41 +0900

    [Bronze IV] Title: 타일 채우기 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15700. 타일 채우기 4/타일 채우기 4.cc
@@ -0,0 +1,10 @@
+// BOJ_15700_타일 채우기 4
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    long long n, m; cin >> n >> m;
+    cout << n * m / 2;
+    return 0;
+}
