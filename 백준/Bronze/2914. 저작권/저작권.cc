82f11335694b37b87df48e501676bc58deebef70
ikk5515 <ikk5515@naver.com>
2023-08-20 22:50:09 +0900

    [Bronze III] Title: 저작권, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2914. 저작권/저작권.cc
@@ -0,0 +1,10 @@
+// BOJ_2914_저작권
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+    int A, I; cin >> A >> I;
+    cout << A * (I - 1) + 1;
+    return 0;
+}
