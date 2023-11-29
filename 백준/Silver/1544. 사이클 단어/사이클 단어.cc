033b50169414750b979e5f12f4f8fd195f2659c2
ikk5515 <ikk5515@naver.com>
2023-11-29 20:39:24 +0900

    [Silver IV] Title: 사이클 단어, Time: 4 ms, Memory: 2424 KB -BaekjoonHub

---
+++ 백준/Silver/1544. 사이클 단어/사이클 단어.cc
@@ -0,0 +1,41 @@
+// BOJ_1544_사이클 단어
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <map>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    int N; cin >> N;
+    map<string, int> m;
+    map<string, int>::iterator iter;
+
+    int ans = 0;
+    for (int i = 0; i < N; i++) {
+        string str; cin >> str;
+
+        iter = m.find(str);
+
+        if (iter == m.end()) {
+            ans++;
+            
+            for (int j = 0; j < str.size(); j++) {
+                string tmp = "";
+                for (int k = j; k < str.size(); k++) {
+                    tmp = tmp + str[k];
+                }
+
+                for (int g = 0; g < j; g++) {
+                    tmp += str[g];
+                }
+
+                m.insert({tmp,i});
+            }
+        } else {
+            continue;
+        }
+    }
+    cout << ans;
+    return 0;
+}
