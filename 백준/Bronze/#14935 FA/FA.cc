adb3624d7123590bb58509f4ebaeb5f3fe3985e4
ikk5515 <ikk5515@naver.com>
2023-12-23 00:36:01 +0900

    [Bronze IV 14935] Title: FA, Time: 0 ms, Memory: 2024 KB -NKLCBHub

---
+++ 백준/Bronze/#14935 FA/FA.cc
@@ -0,0 +1,11 @@
+// BOJ_14935_FA
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    string s; cin >> s;
+    cout << "FA";
+    return 0;
+}
