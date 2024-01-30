b59908090a1f20337b896e52635cb38d79c14da8
ikk5515 <ikk5515@naver.com>
2024-01-30 21:50:54 +0900

    [Bronze IV] Title: Celebrity jeopardy, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/3765. Celebrity jeopardy/Celebrity jeopardy.cc
@@ -0,0 +1,13 @@
+// BOJ_3765_Celebrity jeopardy
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    string s;
+    while (getline(cin, s)) {
+        cout << s << "\n";
+    }
+    return 0;
+}
