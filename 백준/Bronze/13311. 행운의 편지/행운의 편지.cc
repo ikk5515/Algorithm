119d20be3a5b3ab124cdbb2fbc9f60657e2ffa93
ikk5515 <ikk5515@naver.com>
2024-03-15 23:48:57 +0900

    [Bronze III] Title: 행운의 편지, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/13311. 행운의 편지/행운의 편지.cc
@@ -0,0 +1,9 @@
+// BOJ_13311_행운의 편지
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    cout << -1;
+    return 0;
+}
